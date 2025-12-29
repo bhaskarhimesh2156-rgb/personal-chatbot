0ada60f4f4d2a109fe820f3d5d434c9752dd9dbe
bhaskarhimesh2156-rgb <bhaskarhimesh2156@hotmail.com>
2025-12-29 10:06:57 +0530

    Implement basic Flask app with chat functionality

---
+++ app.py
@@ -0,0 +1,22 @@
+from google import genai
+from flask import Flask,request,jsonify,render_template
+client=genai.Client(api_key='AIzaSyArWGM1thYqoTBiUL8XxFbTSpLGidpilbk')
+app= Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/chat", methods=["POST"])
+def chat():
+    # Get user message from request
+    prompt = request.json["message"]
+
+    # Send prompt to Gemini model
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    # Return model response
+    return jsonify({"reply": response.text})
+
+app.run(port=8080)
